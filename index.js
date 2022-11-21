987aa8dad24400928f566d13f809e4a4b8cf0c46
Bob Oyier <boboyier92@gmail.com>
2022-11-21 13:09:49 +0300

    "Completed Moringa School's review strings lab"

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+
+let currentUser = "Bob";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
